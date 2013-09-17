6fa62856dcfa8c50d5dff948ba83efd2f3dc4651
Sarah Pitts <mccallsmedia@gmail.com>
2013-09-17 12:06:29 -0500

    adds collections

--- personal_chef.rb
+++ personal_chef.rb
@@ -1,6 +1,9 @@
 class PersonalChef
-  def make_toast
-    puts "Making your toast!"
+  def make_toast(quantity)
+    quantity.times do
+      puts "Making toast."
+    end
+    puts "I'm done!"
     return self
   end
   def make_eggs(how)
@@ -11,18 +14,33 @@ class PersonalChef
     puts "Making your Orange Juice!"
     return self
   end
-  def good_morning
-    require 'date'
-    today = Date.today.strftime("%A")
-    day_of_year = Date.today.yday
-    puts "Happy #{today}! It is day #{day_of_year} of the year."
+  def gameplan(meals)
+    meals.each do |meal|
+      puts "We'll have #{meal}..."
+    end
+
+    all_meals = meals.join(", ")
+    puts "In summary: #{all_meals}"
     return self
+  end
+  def inventory
+    produce = {apples: 3, oranges: 1, carrots: 12}
+    produce.each do |item, quantity|
+      puts "There are #{quantity} #{item} in the fridge."
+    end
   end
 end
 
 class Butler
   def open_door(which)
     puts "Opening your #{which} door!"
+    return self
+  end
+  def good_morning
+    require 'date'
+    today = Date.today.strftime("%A")
+    day_of_year = Date.today.yday
+    puts "Happy #{today}! It is day #{day_of_year} of the year."
     return self
   end
 end
